cde5e120b2652fa93ac15a381f20896f98b4fbf5
Kulbeer Singh <ksediter1@gmail.com>
2025-06-11 15:52:03 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,30 @@
+
+// about container
+
+ScrollReveal().reveal(".about__content .section__header", {
+
+  ...scrollRevealOption,
+
+});
+
+
+
+ScrollReveal().reveal(".about__content .section__description", {
+
+  ...scrollRevealOption,
+
+  delay: 500,
+
+});
+
+
+
+ScrollReveal().reveal(".about__content .about__btn", {
+
+  ...scrollRevealOption,
+
+  delay: 1000,
+
+});
+
+
